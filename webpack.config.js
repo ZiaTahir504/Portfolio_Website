697283de3b40ffe71ef52e782a172423e4f9146d
ZiaTahir504 <mariatahir504@gmail.com>
2021-01-13 14:49:56 -0600

    (feat) boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+const distPath = path.join(__dirname, './client/dist');
+const srcPath = path.join(__dirname, './client/src/Index.jsx');
+
+module.exports = {
+    mode: 'production',
+    entry: srcPath,
+    output: {
+      path: distPath,
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+        {
+          test: /\.(css)$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    devtool: 'eval-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      fallback: {
+        path: require.resolve('path-browserify'),
+        fs: false,
+      },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(dotenv.config().parsed),
+      }),
+    ],
+  };
+  
