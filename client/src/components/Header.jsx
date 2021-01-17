ba3e4ca50c13bb5733d6f9ab8814a6bb5b9b9521
ZiaTahir504 <mariatahir504@gmail.com>
2021-01-17 12:37:53 -0600

    refactored header to use grid

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import './css/header.css';
+import Grid from "@material-ui/core/Grid";
 
 import myPhoto from '../../assets/myPhoto.png';
 
 const Header = () => {
     return (
         <div id="elementOne">
-            <div id="headerParentContainer">
-                <header id="headerTextContainer">
-                    <h1 id="myName">Hey, I'm Zia</h1>
-                    <h3 id="myStatusOne">full time developer</h3>
-                </header>
-                <div id="imageContainer">
-                    <img id="myPhoto" src={myPhoto}></img>
-                </div>
-            </div>
-            <h3 id="myStatusTwo">part time doodler</h3>   
+            <Grid container spacing={1} id="headerParentContainer">
+                <Grid item xs={12} md={6}>
+                    <header id="headerTextContainer">
+                        <h1 id="myName">Hey, I'm Zia</h1>
+                        <h3 id="myStatusOne">full time developer</h3>
+                        <h3 id="myStatusTwo">part time doodler</h3>   
+                    </header>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <div id="imageContainer">
+                        <img id="myPhoto" src={myPhoto}></img>
+                    </div>
+                </Grid>
+            </Grid>
         </div> 
     ); 
 }
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,75 +1,28 @@
 import React from 'react';
 import './css/projects.css'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import Grid from "@material-ui/core/Grid";
 
 import projectOneImage from '../../assets/barkpoint.png';
 import projectTwoImage from '../../assets/logo.png';
 
-// import { IconButton } from '@material-ui/core';
-// import TwitterIcon from '@material-ui/icons/Twitter';
-// import LinkedInIcon from '@material-ui/icons/LinkedIn';
-// import GitHubIcon from '@material-ui/icons/GitHub';
-
-// <nav id="navButtonsContainer">
-//                 <div id ="buttons">
-//                      <IconButton>
-//                          <TwitterIcon />
-//                      </IconButton>
-//                      <IconButton>
-//                          <LinkedInIcon />
-//                      </IconButton>
-//                      <IconButton>
-//                          <GitHubIcon />
-//                      </IconButton>
-//                 </div>
-//             </nav>
-
 const Projects = () => {
-    return (  
-        <div id="cardsContainer">
-            <div id="cardOneHolder">
-                <Card id="projectOne">
-                    <CardActionArea>
-                        <CardMedia
-                            className="projectImage"
-                            image={projectOneImage}
-                        />
-                        <CardContent>
-                            <text className="projectTitle">Bark Point</text>
-                            <br />
-                            <text className="projectAbout">About Bark Point</text>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button>repo on github</Button>
-                    </CardActions>
-                </Card>
-            </div>
-            <div id='cardTwoHolder'>
-                <Card id="projectTwo">
-                    <CardActionArea>
-                        <CardMedia
-                            className="projectImage"
-                            image={projectTwoImage}
-                        />
-                        <CardContent>
-                            <text className="projectTitle">Babili</text>
-                            <br />
-                            <text className="projectAbout">About Babili</text>
-                        </CardContent>
-                    </CardActionArea>
-                    <CardActions>
-                        <Button>repo on github</Button>
-                    </CardActions>
-                </Card>
-            </div>
-        </div>
-    )
+    return ( 
+        <text>projects</text>
+        // <div id="elementTwo">
+        //     <Grid container spacing={2}>
+        //         <Grid item xs={12} sm={6} md={4}>
+        //             <div className="project">
+        //                 <h1>Bark Point</h1>
+        //             </div>
+        //         </Grid>
+        //         <Grid item xs={12} sm={6} md={4}>
+        //             <div className="project">
+        //                 <h1>Babili</h1>
+        //             </div>
+        //         </Grid>
+        //     </Grid>
+        // </div>  
+    );
 }
 
 export default Projects;
