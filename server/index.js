10ceed1cc2a8cd9c199893172796ac7c648f3532
ZiaTahir504 <mariatahir504@gmail.com>
2021-01-18 19:27:02 -0600

    index somehow was deleted

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT
 const distPath = path.join(__dirname, '../client/dist');
 
 app.use(express.json());
 app.use(express.static(distPath));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.send(path.join(distPath, 'index.html'));
 });
 
