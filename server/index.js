91426d0984838ae3552b8bb3cd047b29b519b51a
ZiaTahir504 <mariatahir504@gmail.com>
2021-01-18 20:17:19 -0600

    deleted babelrc

--- server/index.js
+++ server/index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const path = require('path');
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
+const distPath = path.join(__dirname, '../client/dist');
 
 app.use(express.json());
-app.use(express.static('client\dist\index.html'));
+app.use(express.static(distPath));
 
-app.get('*', (req, res) => {
-  res.send('client\dist\index.html');
+app.get('/', (req, res) => {
+  res.render(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, () => {
