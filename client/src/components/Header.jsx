ff92ab194a3314353d00e0b55f759e2bf24e7ff9
ZiaTahir504 <mariatahir504@gmail.com>
2021-01-19 16:16:04 -0600

    rounded image and added navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import './css/header.css';
 import Grid from "@material-ui/core/Grid";
+import { IconButton } from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import EmailIcon from '@material-ui/icons/Email';
 
-import myDrawing from "../../assets/me.png";
+import myDrawing from "../../assets/version2.png";
 
 const Header = () => {
     return (
         <div id="elementOne">
+            <nav id="navBar">
+                <IconButton>
+                    <GitHubIcon 
+                        style={{ color: 'white' }}
+                        onClick={() => window.open('https://github.com/ZiaTahir504', '_blank')}
+                    />
+                </IconButton> 
+                <IconButton>
+                    <LinkedInIcon 
+                        style={{ color: 'white' }} 
+                        onClick={() => window.open('https://www.linkedin.com/in/zia-tahir-814a01203/', '_blank')}
+                    />
+                </IconButton>
+                <IconButton>
+                    <TwitterIcon 
+                        style={{ color: 'white' }} />
+                </IconButton>
+                <IconButton>
+                    <EmailIcon 
+                        style={{ color: 'white' }} 
+                        onClick={() => location.href="mailto:mariatahir504@gmail.com"}
+                        />
+                </IconButton>
+            </nav>
             <Grid container id="headerParentContainer" spacing={0}>
                 <Grid item xs={12} md={6}>
                     <header id="headerTextContainer">
