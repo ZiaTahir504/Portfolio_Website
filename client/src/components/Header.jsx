cd35246819d81ba77eea2ebc3113b532e21dcf5e
ZiaTahir504 <mariatahir504@gmail.com>
2021-06-04 12:35:03 -0500

    fixed header color issue and white text

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import './css/header.css';
 import Grid from "@material-ui/core/Grid";
-import { IconButton } from '@material-ui/core';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import EmailIcon from '@material-ui/icons/Email';
 
 import myDrawing from "../../assets/version2.png";
 
@@ -15,7 +10,7 @@ const Header = () => {
             <Grid container id="headerParentContainer" spacing={0}>
                 <Grid item xs={12} md={6}>
                     <header id="headerTextContainer">
-                        <h1 id="myName">Hey, I'm Zia</h1>
+                        <h1 id="myName">I'm Maria</h1>
                         <h3 id="myStatusOne">full-time developer</h3>
                         <h3 id="myStatusTwo">part-time doodler</h3>   
                     </header>
